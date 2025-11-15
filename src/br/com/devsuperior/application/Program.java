70ba3f0c0069912e9668e1981319dfbc64e6eb0d
franciscgg <francreationss@gmail.com>
2025-11-15 13:24:46 -0300

    refactor: melhorar organização do fluxo de cadastro e turnos

--- src/br/com/devsuperior/application/Program.java
+++ src/br/com/devsuperior/application/Program.java
@@ -2,6 +2,8 @@ package br.com.devsuperior.application;
 
 import br.com.devsuperior.entities.Champion;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -9,57 +11,46 @@ public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        Champion champion = new Champion();
-        Champion champion2 = new Champion();
+        List<Champion> champions = new ArrayList<>();
 
-        System.out.println("Digite os dados do primeiro campeao: ");
-        System.out.print("Nome: ");
-        String name = sc.nextLine();
-        champion.setName(name);
-        System.out.print("Vida inicial: ");
-        int life = sc.nextInt();
-        champion.setLife(life);
-        System.out.print("Ataque: ");
-        int attack = sc.nextInt();
-        champion.setAttack(attack);
-        System.out.print("Armadura: ");
-        int armor = sc.nextInt();
-        champion.setArmor(armor);
-        sc.nextLine();
-
-        System.out.println("Digite os dados do segundo campeao 2: ");
-        System.out.print("Nome: ");
-        name = sc.nextLine();
-        champion2.setName(name);
-        System.out.print("Vida inicial: ");
-        life = sc.nextInt();
-        champion2.setLife(life);
-        System.out.print("Ataque: ");
-        attack = sc.nextInt();
-        champion2.setAttack(attack);
-        System.out.print("Armadura: ");
-        armor = sc.nextInt();
-        champion2.setArmor(armor);
+        for (int i = 1; i <= 2; i++) {
+            Champion champion = new Champion();
+            System.out.println("Digite os dados do" + i + " campeao:");
+            System.out.print("Nome: ");
+            champion.setName(sc.nextLine());
+            System.out.print("Vida inicial: ");
+            champion.setLife(sc.nextInt());
+            System.out.print("Ataque: ");
+            champion.setAttack(sc.nextInt());
+            System.out.print("Armadura: ");
+            champion.setArmor(sc.nextInt());
+            System.out.println();
+            sc.nextLine();
+            champions.add(champion);
+        }
 
         System.out.print("Quantos turnos voce deseja executar? ");
         int turno = sc.nextInt();
         System.out.println();
 
-        for (int i = 1; i <= turno; i++) {
+        Champion champion1 = champions.get(0);
+        Champion champion2 = champions.get(1);
 
+        for (int i = 1; i <= turno; i++) {
             System.out.printf("Resultado do turno %d:\n", i);
 
-            champion.takeDamage(champion2);
-            champion2.takeDamage(champion);
+            champion1.takeDamage(champion2);
+            champion2.takeDamage(champion1);
 
-            System.out.println(champion.status());
+            System.out.println(champion1.status());
             System.out.println(champion2.status());
             System.out.println();
 
-            if (champion.getLife() == 0 || champion2.getLife() == 0) {
+            if (champion1.getLife() == 0 || champion2.getLife() == 0) {
                 break;
             }
         }
         System.out.println("FIM DO COMBATE!");
+        sc.close();
     }
 }
